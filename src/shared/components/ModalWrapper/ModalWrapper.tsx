f4e276457241b259a8ea6fa05d2b1114a928f95a
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-12-16 18:53:55 -0300

    feat: make dices roll

--- src/shared/components/ModalWrapper/ModalWrapper.tsx
+++ src/shared/components/ModalWrapper/ModalWrapper.tsx
@@ -41,12 +41,12 @@ export const ModalWrapper = ({
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
+                width="42"
+                height="42"
                 fill="#cccccc80"
                 viewBox="0 0 256 256"
                 onClick={() => onStatusChange("close")}
-                className="absolute right-2 top-2 cursor-pointer rounded-full"
+                className="absolute right-0 top-0 bg-background p-1 rounded cursor-pointer"
               >
                 <path d="M165.66,101.66,139.31,128l26.35,26.34a8,8,0,0,1-11.32,11.32L128,139.31l-26.34,26.35a8,8,0,0,1-11.32-11.32L116.69,128,90.34,101.66a8,8,0,0,1,11.32-11.32L128,116.69l26.34-26.35a8,8,0,0,1,11.32,11.32ZM232,128A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z"></path>
               </svg>
