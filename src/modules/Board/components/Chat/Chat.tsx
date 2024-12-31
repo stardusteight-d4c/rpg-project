03a4cdded83f155a7f6d0215c9edf9e7b2b4f00f
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-12-31 18:17:36 -0300

    chore: add handle scroll on chat

--- src/modules/Board/components/Chat/Chat.tsx
+++ src/modules/Board/components/Chat/Chat.tsx
@@ -1,12 +1,51 @@
+"use client"
+
 import { GlowingWrapper } from "@/shared/components"
 import { Sender } from "../Actions/components"
 import ReactMarkdown from "react-markdown"
 import rehypeRaw from "rehype-raw"
+import { useEffect, useRef, useState } from "react"
 
 export const Chat = () => {
+  const chatRef = useRef<HTMLDivElement>(null)
+  const [showButton, setShowButton] = useState(false)
+
+  const scrollToBottom = () => {
+    if (chatRef.current) {
+      chatRef.current.scrollTo({
+        top: chatRef.current.scrollHeight,
+        behavior: "smooth",
+      })
+    }
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [])
+
+  const handleScroll = () => {
+    if (chatRef.current) {
+      const isAtBottom =
+        chatRef.current.scrollTop + chatRef.current.clientHeight >=
+        chatRef.current.scrollHeight - 120
+      setShowButton(!isAtBottom)
+    }
+  }
+
+  useEffect(() => {
+    const chat = chatRef.current
+    if (chat) chat.addEventListener("scroll", handleScroll)
+    return () => {
+      if (chat) chat.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   return (
-    <section className="w-full h-screen overflow-hidden relative">
-      <div className="p-2 pb-[108px] relative z-[50] space-y-8 no-scrollbar h-screen overflow-y-scroll">
+    <section className="w-full relative h-screen overflow-hidden">
+      <div
+        ref={chatRef}
+        className="p-2 pb-[108px] relative z-[50] space-y-8 no-scrollbar h-screen overflow-y-scroll"
+      >
         <div className="flex flex-col">
           <Sender
             characterUrl="https://imgcdn.stablediffusionweb.com/2024/10/24/23cb7d83-7cac-43f6-8ac8-84c9cd8475a6.jpg"
@@ -46,7 +85,22 @@ Disse que no sonho a voz de sua mãe a chamava para casa, a chamava para ficar c
           </div>
         </div>
       </div>
-
+      {showButton && (
+        <button
+          onClick={scrollToBottom}
+          className="sticky z-[200] bottom-[116px] left-1/2 -translate-x-1/2 w-fit bg-ashes text-white p-1 rounded-full  shadow-md shadow-black/50"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="#FFFFFF"
+            viewBox="0 0 256 256"
+          >
+            <path d="M205.66,149.66l-72,72a8,8,0,0,1-11.32,0l-72-72a8,8,0,0,1,11.32-11.32L120,196.69V40a8,8,0,0,1,16,0V196.69l58.34-58.35a8,8,0,0,1,11.32,11.32Z"></path>
+          </svg>
+        </button>
+      )}
       <div className="h-[100px] bg-background z-[200] flex items-center gap-x-4 p-2 pr-4 bottom-0 inset-x-0 border-t border-border absolute">
         <GlowingWrapper styles="w-full" border="rounded-3xl" inset="0">
           <textarea
@@ -57,6 +111,7 @@ Disse que no sonho a voz de sua mãe a chamava para casa, a chamava para ficar c
             className="p-2 px-4 resize-none overflow-y-scroll no-scrollbar h-[84px] w-full cursor-text hover:brightness-125 flex items-center gap-x-1 line-clamp-1 rounded-3xl shadow-p bg-border/50 border border-border outline-none"
           ></textarea>
         </GlowingWrapper>
+
         <span className="flex hover:brightness-125 active:scale-95 transition-all duration-300 ease-in-out items-center  justify-center text-white p-2 rounded-full w-fit  shadow-md shadow-black/50 cursor-pointer bg-gradient-to-tr from-[#42d392] to-[#8B5CF6]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
