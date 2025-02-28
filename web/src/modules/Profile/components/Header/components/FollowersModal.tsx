5230d1fd6b3fa24d668f6f50b8bcffaa863dcb24
Gabriel Sena <stardusteight.d4cc@gmail.com>
2025-02-28 10:29:25 -0300

    chore: make empty state of followers and following

--- web/src/modules/Profile/components/Header/components/FollowersModal.tsx
+++ web/src/modules/Profile/components/Header/components/FollowersModal.tsx
@@ -37,6 +37,27 @@ export const FollowersModal: React.FC<{
     >
       <div className="border-b pb-2 -mt-2 border-border shadow-md shadow-black/50  w-full z-[200] bg-background"></div>
       <div className="w-[700px] p-2">
+        {followers.length === 0 && (
+          <div className="w-full flex items-center justify-center">
+            <div className="p-8 w-full h-[230px] bg-ashes rounded-xl flex flex-col items-center justify-center">
+              <div className="col-span-1 w-[50px] h-[50px] flex items-center justify-center bg-border/50 border border-border rounded aspect-square">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  fill="#9ca3af"
+                  viewBox="0 0 256 256"
+                >
+                  <path d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z"></path>
+                </svg>
+              </div>
+              <span className="text-gray-400 block mt-2 w-[400px] text-center">
+                The void responds with silence... but one day, the first adept
+                will hear its call.
+              </span>
+            </div>
+          </div>
+        )}
         <div className="space-y-2">
           {followers.map((follower) => (
             <div
--- web/src/modules/Profile/components/Header/components/FollowingModal.tsx
+++ web/src/modules/Profile/components/Header/components/FollowingModal.tsx
@@ -38,6 +38,27 @@ export const FollowingModal: React.FC<{
       <div className="border-b pb-2 -mt-2 border-border shadow-md shadow-black/50  w-full z-[200] bg-background"></div>
 
       <div className="w-[700px] p-2">
+        {following.length === 0 && (
+          <div className="w-full flex items-center justify-center">
+            <div className="p-8 w-full h-[230px] bg-ashes rounded-xl flex flex-col items-center justify-center">
+              <div className="col-span-1 w-[50px] h-[50px] flex items-center justify-center bg-border/50 border border-border rounded aspect-square">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  fill="#9ca3af"
+                  viewBox="0 0 256 256"
+                >
+                  <path d="M240,88.23a54.43,54.43,0,0,1-16,37L189.25,160a54.27,54.27,0,0,1-38.63,16h-.05A54.63,54.63,0,0,1,96,119.84a8,8,0,0,1,16,.45A38.62,38.62,0,0,0,150.58,160h0a38.39,38.39,0,0,0,27.31-11.31l34.75-34.75a38.63,38.63,0,0,0-54.63-54.63l-11,11A8,8,0,0,1,135.7,59l11-11A54.65,54.65,0,0,1,224,48,54.86,54.86,0,0,1,240,88.23ZM109,185.66l-11,11A38.41,38.41,0,0,1,70.6,208h0a38.63,38.63,0,0,1-27.29-65.94L78,107.31A38.63,38.63,0,0,1,144,135.71a8,8,0,0,0,16,.45A54.86,54.86,0,0,0,144,96a54.65,54.65,0,0,0-77.27,0L32,130.75A54.62,54.62,0,0,0,70.56,224h0a54.28,54.28,0,0,0,38.64-16l11-11A8,8,0,0,0,109,185.66Z"></path>
+                </svg>
+              </div>
+              <span className="text-gray-400 block mt-2 w-[400px] text-center">
+                Not even the most insane occultists walk alone, there's always
+                something whispering in the dark.
+              </span>
+            </div>
+          </div>
+        )}
         <div className="space-y-2">
           {following.map((userFollowing) => (
             <div
